5d00e4e3aba217ea1f213f44842b40712dc4fdea
TheLuaFox86 <peanuts005@outlook.com>
2025-06-14 23:57:49 +0000

    Windows Fix

---
+++ Versions/SOURCE_INDEV/TMP/startup/init/linux-ish.lua
@@ -0,0 +1,51 @@
+_G.platform = {}
+local lfs = require "lfs"
+platform.fs = {}
+_G.string.split = function(txt, del)
+	local out = {}
+	local c = 0
+	local a = ""
+	i = 0
+	for _=1, #txt do
+		i = i + 1
+		if txt:sub(i , i + (#del - 1)) == del then
+			c = c + 1
+			out[c] = a
+			a = ""
+			i = i + (#del - 1)
+		else
+			--print(txt:sub(i, i + (#del - 1)))
+			a = a .. txt:sub(i, i)
+		end
+	end
+	out[c + 1] = a
+	return out
+end
+local iowrite = _G.io.write
+_G.io.write = function(...)
+iowrite(...)
+io.flush()
+end
+_G.each = function(a)
+	i = 0
+	return function(a)
+		i = i + 1
+		local v = a[i]
+		if v then
+			return v
+		end
+	end, a
+end
+function platform.fs.listdir(p)
+  local a = {}
+	for b in lfs.dir(p) do
+		if b ~= '.' and b ~= '..' then
+			table.insert(a, b)
+		end
+	end
+	return each(a)
+end
+platform.Zip = {}
+function platform.Zip.extract(file, to)
+  os.execute('unzip -o "' .. file .. '" -d "' .. to .. '"')
+end
