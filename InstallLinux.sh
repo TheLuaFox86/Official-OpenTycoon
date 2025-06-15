7e12c5701865a588fe837329ce5edd80b0948b6d
TheLuaFox86 <peanuts005@outlook.com>
2025-06-15 00:38:27 +0000

    fix linux installer

--- InstallLinux.sh
+++ InstallLinux.sh
@@ -1,16 +1,16 @@
-$WD= $PWD
+WD=$PWD
 cd $HOME
 apk add git
 apk add luarocks5.3
 apk add zip
 apk add openssh
-apk add lfs
+apk add lua-filesystem
 apk add nano
 git clone https://github.com/TheLuaFox86/Official-OpenTycoon.git
 mkdir ./OpenTycoon
 unzip ./Official-OpenTycoon/launcher/ISH-launcher.zip -d ./OpenTycoon
-cp ./Official-OpenTycoon/launcher/start.sh /bin/OT.sh
-chmod +x /bin/OT.sh
+cp ./Official-OpenTycoon/launcher/start.sh /bin/otl
+chmod +x /bin/otl
 mkdir OpenTycoon/Game
 mkdir OpenTycoon/Game/versions
 cp Official-OpenTycoon/Versions/alpha5/Alpha5.zip OpenTycoon/Game/versions
