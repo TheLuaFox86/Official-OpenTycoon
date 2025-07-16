016a54f77bb588ba7e386ae1771034cfa8b8c531
TheLuaFox86 <peanuts005@outlook.com>
2025-07-16 02:06:48 +0000

    hello

--- Versions/SOURCE_INDEV/TMP/startup/classloader.lua
+++ Versions/SOURCE_INDEV/TMP/startup/classloader.lua
@@ -1,32 +1,4 @@
-_G.class =  {}
-function class:append(tb)
-  if not self[tb.domain] then self[tb.domain] = {} end
-  self[tb.domain][tb.type] = tb
-end
-function class:getById(t, id)
-	log(t)
-  local a = {}
-  for i, v in ipairs(id:split(":")) do
-    a[i]=v
-  end
-  if not a[2] then
-    a[2] = a[1]
-    a[1] = "opentycoon"
-  end
-  log(a[1], a[2])
-  return self[a[1]][t][a[2]]
-end
-function class:setById(t, id, val)
-  local a = {}
-  for i, v in ipairs(id:split(":")) do
-    a[i]=v
-  end
-  if not a[2] then
-    a[2] = a[1]
-    a[1] = "opentycoon"
-  end
-  self[a[1]][t][a[2]] = val
- end
+dofile(config.tmpp .. config.di .. "startup" .. config.di .. "classCore.lua")
 log("Plugin PH2:")
 log(pcall(function()
     pluginp = config.tmpp .. "/startup/plugin"
